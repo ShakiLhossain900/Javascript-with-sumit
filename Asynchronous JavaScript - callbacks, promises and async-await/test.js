cde650c41bcd131b7f9b631360c5388fa58bb34a
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-25 23:41:20 +0600

    callbacks

--- Asynchronous JavaScript - callbacks, promises and async-await/test.js
+++ Asynchronous JavaScript - callbacks, promises and async-await/test.js
@@ -1,15 +1,32 @@
-// asynchronous  joto para jay async system code likhte hobe and dekbe hobe code jeno block na hoy
-const processOrder = (customer) => {
-    console.log('processing order for customer 1');
+// // asynchronous  joto para jay async system code likhte hobe and dekbe hobe code jeno block na hoy
+// const processOrder = (customer) => {
+//     console.log('processing order for customer 1');
 
-  setTimeout(() =>{
-    console.log('cooking is completed');
-  },3000);
+//   setTimeout(() =>{
+//     console.log('cooking is completed');
+//   },3000);
 
-  console.log('order process for customer 1');
+//   console.log('order process for customer 1');
+// }
+
+// console.log('take order for customer 1');
+// processOrder();
+// console.log('completed order for customer 1');
+
+
+
+
+
+//asynchronous callbacks              
+const takeOrder = (customer)=> {
+    console.log(`take order for ${customer}`);
 }
 
-console.log('take order for customer 1');
-processOrder();
-console.log('completed order for customer 1');
+const processOrder = (customer) =>{
+    console.log(`processing order for ${customer} 1`);
 
+    setTimeout(()=>{
+        console.log(`cooking completed`);
+    },3000);
+    console.log('order procesed for customer 1');
+}
