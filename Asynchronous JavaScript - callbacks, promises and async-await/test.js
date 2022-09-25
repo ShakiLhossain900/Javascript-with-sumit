8ab0ee6dda77f4edb82bbbc37bf7599e724d426b
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-25 23:18:04 +0600

    oder async function

--- Asynchronous JavaScript - callbacks, promises and async-await/test.js
+++ Asynchronous JavaScript - callbacks, promises and async-await/test.js
@@ -1,1 +1,13 @@
-alert('hi');
+const processOrder = (customer) => {
+    console.log('processing order for customer 1');
+
+  setTimeout(() =>{
+    console.log('cooking is completed');
+  },3000);
+
+  console.log('order process for customer 1');
+}
+
+console.log('take order for customer 1');
+processOrder();
+console.log('completed order for customer 1');
