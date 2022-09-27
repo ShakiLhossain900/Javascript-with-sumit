8c2aebab4780005122752746d931254d796332ac
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-27 21:48:59 +0600

    promise

--- Asynchronous JavaScript - callbacks, promises and async-await/promise.js
+++ Asynchronous JavaScript - callbacks, promises and async-await/promise.js
@@ -1,3 +1,6 @@
+// todays plan to need complete the task of this video seris learn with sumit
+
+//callback ke reduce korar jonnu amara promise use korbu
 const promise = new Promise((resolve, reject)=> {
     
 })
--- Asynchronous JavaScript - callbacks, promises and async-await/test.js
+++ Asynchronous JavaScript - callbacks, promises and async-await/test.js
@@ -1,6 +1,5 @@
 //Synchronous = happens at the same time. Asynchronous = doesn't happen at the same time. With synchronous learning, participants can receive immediate feedback.
 
-
 // // asynchronous  joto para jay async system code likhte hobe and dekbe hobe code jeno block na hoy
 // const processOrder = (customer) => {
 //     console.log('processing order for customer 1');
@@ -16,35 +15,29 @@
 // processOrder();
 // console.log('completed order for customer 1');
 
+//asynchronous callbacks
+const takeOrder = (customer, callback) => {
+  console.log(`take order for ${customer}`);
+  callback(customer);
+};
 
+const processOrder = (customer, callback) => {
+  console.log(`processing order for ${customer} `);
 
+  //scync
+  setTimeout(() => {
+    console.log(`cooking completed`);
+    console.log(`order procesed for ${customer} `);
+    callback(customer);
+  }, 3000);
+};
+const completedOrder = (customer) => {
+  console.log(`completed order ${customer}`);
+};
 
-
-//asynchronous callbacks              
-const takeOrder = (customer,callback) => {
-    console.log(`take order for ${customer}`);
-    callback(customer)
-}
-
-const processOrder = (customer,callback) =>{
-    console.log(`processing order for ${customer} `);
-
-
-    //scync
-    setTimeout(()=>{
-        console.log(`cooking completed`);
-        console.log(`order procesed for ${customer} `);
-        callback(customer)
-    },3000);
-   
-}
-const completedOrder = (customer)=>{
-    console.log(`completed order ${customer}`);
-}
-
-takeOrder('customer 1',(customer)=>{
-    processOrder(customer, (customer)=>{
-        completedOrder(customer)
-    })
-})
+takeOrder("customer 1", (customer) => {
+  processOrder(customer, (customer) => {
+    completedOrder(customer);
+  });
+});
 console.log("hello");
