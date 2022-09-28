76996e5e5e5de71d977bb525f664cc35099ca80a
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-28 22:17:39 +0600

    start todays code

--- Asynchronous JavaScript - callbacks, promises and async-await/promise.js
+++ Asynchronous JavaScript - callbacks, promises and async-await/promise.js
@@ -1,7 +1,20 @@
 // todays plan to need complete the task of this video seris learn with sumit
 
 //callback ke reduce korar jonnu amara promise use korbu
+
+//this video i need to complete tonight  
+
 const hasMeeting = false;
 const metting = new Promise((resolve, reject)=> {
-    
+    if(!hasMeeting) {
+        const mettingDetails = {
+            name: 'Technical Meeting',
+            location: 'Google Meet',
+            time: '10.00 PM'
+        };
+        resolve(mettingDetails);
+    }
+    else {
+        reject(new Error("meeting already scheduled"))
+    }
 })
