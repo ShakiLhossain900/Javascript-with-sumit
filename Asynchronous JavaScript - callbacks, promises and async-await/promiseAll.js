035e5c22e0c2ab43a7d0d5bd37ffdec8a32cdd29
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-29 13:08:28 +0600

    promise array print

---
+++ Asynchronous JavaScript - callbacks, promises and async-await/promiseAll.js
@@ -0,0 +1,14 @@
+const promise1 = Promise.resolve(`Promise 1 resolve`);
+
+const promise2 = new Promise((resolve, reject) =>{
+    setTimeout(() =>{ 
+        resolve(`Promise 2 resolved`)
+    },2000)
+});
+
+//ata aktar por 2 sec por arekta print korbe
+promise1.then((res)=> console.log(res))
+promise2.then((res)=> console.log(res))
+
+//amra jodi 2 ta promise ke aktar por akta prinat na kore akshathe print korte chai taile amader 
+Promise.all([promise1, promise2]).then ((res)=> console.log(res))
