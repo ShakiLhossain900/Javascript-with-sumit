79b3c45c00504c81ebd32b55761b81be372ed7f8
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-29 12:49:56 +0600

    adding another promise

--- Asynchronous JavaScript - callbacks, promises and async-await/promise.js
+++ Asynchronous JavaScript - callbacks, promises and async-await/promise.js
@@ -2,16 +2,15 @@
 
 //callback ke reduce korar jonnu amara promise use korbu
 
-//this video i need to complete tonight
 
-
+//this is one promise 
 const hasMeeting = false;
 const metting = new Promise((resolve, reject) => {
   if (hasMeeting) {
     const MeetingDetails = {
       name: "technical meeting",
       location: "ss-blockChain-it",
-      time: new Date(),
+      time: "10th of december",
     };
 
     resolve(MeetingDetails);
@@ -20,12 +19,18 @@ const metting = new Promise((resolve, reject) => {
   }
 });
 
+//using another promise, return the promise 
+const addToCalender = (meetingDetails) => {
+  const calender = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time} `;
+  return Promise.resolve(calender);
+};
+
 console.log("hey shakil i am started working ");
 metting
-.then((res) => {
-  console.log(JSON.stringify(res));
-})
-.catch((err) => {
+  .then((res) => {
+    console.log(JSON.stringify(res));
+  })
+  .catch((err) => {
     console.log(err.message);
-})
+  });
 console.log("hey i am the last log");
