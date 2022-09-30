0eef8ab520e379b5d6b29ae4cb61d0e6aa9c2fc4
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-30 08:27:14 +0600

    second commit

--- Asynchronous JavaScript - callbacks, promises and async-await/async-await.js
+++ Asynchronous JavaScript - callbacks, promises and async-await/async-await.js
@@ -1,4 +1,7 @@
- function friendlyFunciton() { 
+//first function ta chilo sync than amara async keyword use kore ata async korlam tokhon ata akta promise er vitor akta object return korlo
+async function friendlyFunciton() { 
+    //return Promise.resolve('hello shakil!');
     return `hello`;
+  
 }
-console.log(friendlyFunciton());
+console.log(friendlyFunciton()); 
