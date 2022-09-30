8ca22ee711fe4fa7da0af8893826d4ca212fac2f
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-30 08:38:32 +0600

    okey done

--- Asynchronous JavaScript - callbacks, promises and async-await/async-await.js
+++ Asynchronous JavaScript - callbacks, promises and async-await/async-await.js
@@ -1,7 +1,55 @@
-//first function ta chilo sync than amara async keyword use kore ata async korlam tokhon ata akta promise er vitor akta object return korlo
-async function friendlyFunciton() { 
-    //return Promise.resolve('hello shakil!');
-    return `hello`;
+// //first function ta chilo sync than amara async keyword use kore ata async korlam tokhon ata akta promise er vitor akta object return korlo
+// async function friendlyFunciton() { 
+//     //return Promise.resolve('hello shakil!');
+//     return `hello`;
   
+// }
+// console.log(friendlyFunciton()); 
+
+
+
+//practice async await 
+//this is one promise 
+const hasMeeting = false;
+const meeting = new Promise((resolve, reject) => {
+  if (!hasMeeting) {
+    const MeetingDetails = {
+      name: "technical meeting",
+      location: "ss-blockChain-it",
+      time: "10th of december",
+    };
+
+    resolve(MeetingDetails);
+  } else {
+    reject(new Error("Invalid meeting!"));
+  }
+});
+
+//using another promise, return the promise 
+const addToCalender = (meetingDetails) => {
+  const calender = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time} `;
+  return Promise.resolve(calender);
+};
+
+
+// meeting
+//   .then(addToCalender)
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+// console.log("hello");
+
+async function myMeeting (){
+    // awit means here i need to wait meeting er data golo powa porjonto calender y jaite parbe na 
+    //than calender await y means addTocalender y data na asa poorjonto log korbe na 
+    console.log("starting");
+    const meetingDetails = await meeting;
+    const calender = await addToCalender(meetingDetails);
+    console.log(calender);
+    console.log("end");
 }
-console.log(friendlyFunciton()); 
+
+myMeeting()
